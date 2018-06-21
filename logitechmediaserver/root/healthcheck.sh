79ddd3d7add8c97221b918f8068d783948c06d61
eyjhbb@gmail.com <eyjhbb@gmail.com>
2018-06-21 20:37:46 +0200

    fixed healthcheck for logitechmediaserver

--- logitechmediaserver/root/healthcheck.sh
+++ logitechmediaserver/root/healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 /bin/echo [HEALTHCHECK]
 
-/bin/echo -e "GET / HTTP/1.1\n\n" | nc 127.0.0.1 9000 | grep -i logitech
+/bin/echo -e "GET / HTTP/1.1\n\n" | nc -w 2 127.0.0.1 9000 | grep -i logitech
 
 return $?
